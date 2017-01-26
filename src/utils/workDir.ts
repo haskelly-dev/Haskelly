67b9699fd32cb72a4a917cc5a76541e58ceb9bc8
Marco Concetto Rudilosso <marcoconcettorudilosso@gmail.com>
2017-01-26 15:55:52 +0000

    working on windows

--- src/utils/workDir.ts
+++ src/utils/workDir.ts
@@ -1,10 +1,16 @@
 import { execSync } from 'child_process';
 import { dirname, resolve } from 'path'
 export function getWorkDir(filepath) {
-    try {
+     try {
         const path = execSync("stack query", {cwd: dirname(filepath)}).toString();
-        const re = /.*path: ([^\n]*)\n.*/
-        return {cwd: re.exec(path)[1]};
+        const re = /.*path:\s([^\n\s]*)\s.*/
+        var extract = re.exec(path)[1];
+        const reWindows = /C:(\\.*\\)*/
+        if (reWindows.test(extract)){
+            extract = extract.replace(/\\/g, "/");
+            extract = extract.slice(2, extract.length)
+        }
+        return {cwd: extract};
     } catch (e) {
         return {};
     }
